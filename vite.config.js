a76b382020388d171e41d367dd4618567481eea3
slavojanowski <slavojanowski@gmail.com>
2024-06-23 09:39:10 +0200

    starting new project - from old repository

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "01_Dzien_1/03_Podstawowe_selektory/02_Zadanie_2";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
